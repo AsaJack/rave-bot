dc7753bf95047c2e150f368f918709677aad23df
Asa Jack Marsh <97415731+AsaJack@users.noreply.github.com>
2023-04-13 12:14:17 -0700

    Update index.js
    Updated comments and documentation.

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
+// Import necessary modules and libraries
 const { Client, Intents } = require('discord.js');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource} = require('@discordjs/voice');
-
-const { token } = require("./config.json");
 const { join } = require('node:path');
+const { token } = require("./config.json");
 
-// Audio file resource used by Rave Bot
+// Define the audio file resource used by Rave Bot
 let resource = createAudioResource(join(__dirname, 'rave.mp3'));
 
-// Create bot
+// Create Discord bot client with required intents
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES] });
 
-// Log in console when the bot goes online
+// Log a message when the bot is ready
 client.once("ready", () => {
   console.log("Ready!");
 });
 
-/ Define the event for when a user joins a voice channel
+// Define the event for when a user joins or leaves a voice channel
 client.on('voiceStateUpdate', async (oldState, newState) => {
+  // If a user joins a voice channel
   if (newState.channel) {
-    // User joined or switched to a new a voice channel
+    // Get the voice channel the user has joined
     const voiceChannel = newState.channel;
+
+    // Connect to the voice channel
     const connection = joinVoiceChannel({
       channelId: voiceChannel.id,
       guildId: voiceChannel.guild.id,
       adapterCreator: voiceChannel.guild.voiceAdapterCreator,
     });
 
-    // Load and play the audio file for 5 seconds
+    // Create an audio player and subscribe to the voice connection
     const player = createAudioPlayer();
     connection.subscribe(player);
+
+    // Play the audio file resource for 5 seconds
     player.play(resource);
     await new Promise(resolve => player.on('idle', resolve));
     await new Promise(resolve => setTimeout(resolve, 5000));
@@ -38,5 +43,5 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
   }
 });
 
-// Log bot into discord with private token
+// Log the bot in with the provided token
 client.login(token);
