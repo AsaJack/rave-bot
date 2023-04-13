f997790d24cf6eed6444e14837fa8dc07628fbd4
Asa Jack Marsh <97415731+AsaJack@users.noreply.github.com>
2023-04-13 12:00:46 -0700

    Update index.js
    Grouped modules that require the same library.
    Removed unnecessary intents.
    Restructured voice state event handler to be asynchronous and include the case where user switch voice channels.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const { Client, Intents } = require('discord.js');
-const { joinVoiceChannel } = require('@discordjs/voice');
-const { createAudioPlayer } = require('@discordjs/voice');
-const { createAudioResource } = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource} = require('@discordjs/voice');
 
 const { token } = require("./config.json");
 const { join } = require('node:path');
@@ -10,43 +8,34 @@ const { join } = require('node:path');
 let resource = createAudioResource(join(__dirname, 'rave.mp3'));
 
 // Create bot
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES] });
 
 // Log in console when the bot goes online
 client.once("ready", () => {
   console.log("Ready!");
 });
 
-// Code triggers when a discord member joins a new voice channel
-client.on('voiceStateUpdate', (oldState, newState) => {
-  if (oldState.channelId === null) {
-
-    // Log in console that a rave has started
-    console.log('Rave');
-
-    // Create an audio player with a connection to the voice channel that was joined by the discord member and then plays
-    // the crab rave audio on that channel for 5 seconds.
-    const player = createAudioPlayer();
-
+/ Define the event for when a user joins a voice channel
+client.on('voiceStateUpdate', async (oldState, newState) => {
+  if (newState.channel) {
+    // User joined or switched to a new a voice channel
+    const voiceChannel = newState.channel;
     const connection = joinVoiceChannel({
-      channelId: newState.channel.id,
-      guildId: newState.guild.id,
-      adapterCreator: newState.guild.voiceAdapterCreator
+      channelId: voiceChannel.id,
+      guildId: voiceChannel.guild.id,
+      adapterCreator: voiceChannel.guild.voiceAdapterCreator,
     });
 
+    // Load and play the audio file for 5 seconds
+    const player = createAudioPlayer();
+    connection.subscribe(player);
     player.play(resource);
+    await new Promise(resolve => player.on('idle', resolve));
+    await new Promise(resolve => setTimeout(resolve, 5000));
 
-    const subscription = connection.subscribe(player);
-
-    setTimeout(() => {
-      //subscription.unsubscribe();
-      try {
-        connection.destroy();
-      } catch (error) {
-
-      }
-    }, 5_000);
-  };
+    // Disconnect from the voice channel
+    connection.destroy();
+  }
 });
 
 // Log bot into discord with private token
