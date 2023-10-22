eb6baaca7f3ac01d4045f3f2fcd04bf968316b76
Asa Jack Marsh <97415731+AsaJack@users.noreply.github.com>
2023-10-22 12:39:36 -0700

    Discord.js Update
    Updated code to work with new discord.js updates.

--- index.js
+++ index.js
@@ -1,47 +1,105 @@
-// Import necessary modules and libraries
-const { Client, Intents } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource} = require('@discordjs/voice');
+// Import necessary modules and functions from discord.js and @discordjs/voice packages
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
 const { join } = require('node:path');
 const { token } = require("./config.json");
 
-// Define the audio file resource used by Rave Bot
-let resource = createAudioResource(join(__dirname, 'rave.mp3'));
+// Define the path to the audio resource file
+const RESOURCE_PATH = join(__dirname, 'rave.mp3');
 
-// Create Discord bot client with required intents
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES] });
-
-// Log a message when the bot is ready
-client.once("ready", () => {
-  console.log("Ready!");
+// Create a new Discord client with specified intents
+const client = new Client({
+  intents: [
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ]
 });
 
-// Define the event for when a user joins or leaves a voice channel
-client.on('voiceStateUpdate', async (oldState, newState) => {
-  // If a user joins a voice channel
-  if (newState.channel) {
-    // Get the voice channel the user has joined
-    const voiceChannel = newState.channel;
+// Function to create a voice connection to a Discord channel
+const createVoiceConnection = (channelId, guildId, adapterCreator) => {
+  return joinVoiceChannel({
+    channelId,
+    guildId,
+    adapterCreator
+  });
+};
 
-    // Connect to the voice channel
-    const connection = joinVoiceChannel({
-      channelId: voiceChannel.id,
-      guildId: voiceChannel.guild.id,
-      adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-    });
+// Function to set up the audio player and play the specified audio resource in the given connection
+const setupPlayer = (connection) => {
+  // Create an audio resource from the specified file path
+  const AUDIO_RESOURCE = createAudioResource(RESOURCE_PATH)
+  // Create an audio player
+  const player = createAudioPlayer();
+  // Play the audio resource in the player
+  player.play(AUDIO_RESOURCE);
+  // Subscribe the player to the connection
+  connection.subscribe(player);
+  // Return the player for further control if needed
+  return player;
+};
 
-    // Create an audio player and subscribe to the voice connection
-    const player = createAudioPlayer();
-    connection.subscribe(player);
+// Function to clean up the voice connection after a specified timeout
+const cleanupConnection = (connection) => {
+  // Set a timeout of 5 seconds for cleanup
+  const timeout = setTimeout(() => {
+    try {
+      // Destroy the voice connection
+      connection.destroy();
+      // Clear the timeout to prevent it from triggering after cleanup
+      clearTimeout(timeout);
+      // Log a message indicating successful cleanup
+      console.log("Voice connection cleaned up successfully");
+    } catch (error) {
+      // Handle any errors that occur during cleanup and log the error
+      console.error('Error occurred during cleanup:', error);
+    }
+  }, 5000); // 5000 milliseconds (5 seconds)
+};
 
-    // Play the audio file resource for 5 seconds
-    player.play(resource);
-    await new Promise(resolve => player.on('idle', resolve));
-    await new Promise(resolve => setTimeout(resolve, 5000));
+// Event handler when the bot is ready
+client.once("ready", () => {
+  // Log a message when the bot is ready
+  console.log("Bot is ready!");
+});
 
-    // Disconnect from the voice channel
-    connection.destroy();
+// Event handler for voice state updates
+client.on('voiceStateUpdate', (oldState, newState) => {
+  // Check if the updated voice state is related to the bot user
+  if (newState.member.user.id === client.user.id) {
+    // If it's the bot's own voice state update, do nothing
+    return;
+  }
+
+  // Store the previous and current voice states for convenience
+  const previousVoiceState = oldState;
+  const currentVoiceState = newState;
+
+  // Check if the bot has just joined a voice channel (previous channel ID is null)
+  if (previousVoiceState.channelId === null) {
+    // Log a message indicating that the bot has joined a voice channel
+    console.log('Bot has joined a voice channel');
+    try {
+      // Create a voice connection to the current channel
+      const connection = createVoiceConnection(
+        currentVoiceState.channel.id,
+        currentVoiceState.guild.id,
+        currentVoiceState.guild.voiceAdapterCreator
+      );
+
+      // Set up the player and play the audio resource in the connection
+      const player = setupPlayer(connection);
+      
+      // Clean up the voice connection after playback (5 seconds in this case)
+      cleanupConnection(connection);
+    } catch (error) {
+      // Handle any errors that occur during voice connection setup and log the error
+      console.error('Error occurred during voice connection setup:', error);
+    }
   }
 });
 
-// Log the bot in with the provided token
+// Log in to Discord using the provided bot token from the configuration file
 client.login(token);
